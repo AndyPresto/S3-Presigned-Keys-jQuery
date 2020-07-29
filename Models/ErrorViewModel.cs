8e33300e8ad7d84315f91adaf95ee6276791539d
Andy Preston <andy@kentweb.co.uk>
2020-07-29 22:10:12 +0700

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace S3_Presigned_Keys_jQuery.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
